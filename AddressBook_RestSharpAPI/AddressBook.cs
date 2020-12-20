6d7f963b2945c9331945fee8b68244d0eac07a88
Bhagi <puppyreddy1997@gmail.com>
2020-12-20 15:51:57 +0530

    [Bhagyalaxmi]ADDED:Retrieving The Persons Count Using GET

--- AddressBook_RestSharpAPI/AddressBook.cs
+++ AddressBook_RestSharpAPI/AddressBook.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace AddressBook_RestSharpAPI
 {
-    class AddressBook
+    public class AddressBook
     {
+        public int id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
--- AddressBook_RestSharpAPI/UnitTest1.cs
+++ AddressBook_RestSharpAPI/UnitTest1.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
 
 namespace AddressBook_RestSharpAPI
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        RestClient client;
+
+        [TestInitialize]
+        public void setUp()
         {
+            client = new RestClient(" http://localhost:3000");
+        }
+        private IRestResponse getAddressBookList()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/addressBook", Method.GET);
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        [TestMethod]
+        public void whileCallingGETAPi_shouldReturnAddressBookList()
+        {
+            IRestResponse response = getAddressBookList();
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<AddressBook> dataResponse = JsonConvert.DeserializeObject<List<AddressBook>>(response.Content);
+            Assert.AreEqual(4, dataResponse.Count);
         }
     }
 }
